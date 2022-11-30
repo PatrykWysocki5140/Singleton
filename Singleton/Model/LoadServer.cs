41f15eecda7d1f9debfb7f4804515e13dc799f65
PatrykWysocki5140 <56190743+PatrykWysocki5140@users.noreply.github.com>
2022-11-30 14:49:24 +0100

    Add files via upload

---
+++ Singleton/Model/LoadServer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Model
+{
+        public sealed class LoadServer
+    {
+
+        private static readonly LoadServer instance = new LoadServer();
+        private readonly List<Server> servers= new List<Server> ();
+        private readonly Server defaut = new Server { Name = "example server name", IP = "1.1.1.1" };
+        private readonly Random random = new Random();
+
+        public int Count;
+        private LoadServer()
+        {
+            
+            for(int i = 0; i < 100; i++)
+            {
+                if (i==0 ) { servers.Add(defaut); }
+
+                servers.Add(new Server { Name = i.ToString(), IP = $"{random.Next(224)}.{random.Next(224)}.{random.Next(224)}.{random.Next(224)}" });
+            }
+        }
+        public static LoadServer GetLoadServer()
+        {
+            return instance;
+        }
+
+        
+        public Server RandomServer
+        {
+            get
+            {
+                int r = random.Next(servers.Count);
+                if (r==0)
+                {
+                    return defaut;
+                }
+                else
+                {
+                    return servers[r];
+                }
+            }
+        }
+
+        public List<Server> AllServers
+        {
+            get { return servers; }
+        }
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿using Singleton.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] a)
+        {         
+            var server = LoadServer.GetLoadServer();
+            
+
+            Console.WriteLine("All servers\n\n\n" );
+            var servList = server.AllServers.ToList();
+            foreach(var obj in servList)
+            {
+                Console.WriteLine($"Server: {obj.Name}");
+                Console.WriteLine($"                       IP: {obj.IP}");
+                Console.WriteLine("----------------------------------------");
+            }
+
+
+            Console.WriteLine("\n\n\n##############################################\n\n\n" );
+
+
+            Console.WriteLine("Random servers\n\n\n" );
+            for (int i = 0; i < 15; i++)
+            {
+                Server serv = server.RandomServer;
+
+                Console.WriteLine($"Server: {serv.Name}" );
+                Console.WriteLine($"                       IP: {serv.IP}" );
+                Console.WriteLine("----------------------------------------" );
+            }
+            
+            Console.WriteLine("\n\n\n##############################################\n\n\n" );
+            Console.ReadKey();
+        }
+    }
+}
